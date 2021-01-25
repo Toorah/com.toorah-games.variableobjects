8fe661ee84d0c66c4b240446ba150e77e4a9f252
Toorah <tugratoorah@gmail.com>
2021-01-26 00:33:28 +0100

    Added Animation Curve Variables

---
+++ Runtime/Definitions/AnimationCurveListVariable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Toorah.ScriptableVariables
+{
+    [CreateAssetMenu(fileName = "AnimationCurve List Variable", menuName = "Scriptable Variables/List/AnimationCurve")]
+    public class AnimationCurveListVariable : ListVariable<AnimationCurve>
+    {
+
+    }
+}
---
+++ Runtime/Definitions/AnimationCurveVariable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Toorah.ScriptableVariables
+{
+    [CreateAssetMenu(fileName = "AnimationCurve Variable", menuName = "Scriptable Variables/Single/AnimationCurve")]
+    public class AnimationCurveVariable : ScriptableVariable<AnimationCurve>
+    {
+
+    }
+}
