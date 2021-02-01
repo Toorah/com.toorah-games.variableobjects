c98d8062662902a63d30839c101c5665b5306be8
Toorah <tugratoorah@gmail.com>
2021-02-01 19:15:00 +0100

    Added Quaterion Variable
    Added new Quaternion Variable

---
+++ Runtime/Definitions/QuaternionListVariable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Toorah.ScriptableVariables
+{
+    [CreateAssetMenu(fileName = "Quaternion List Variable", menuName = "Scriptable Variables/List/Quaternion")]
+    public class QuaternionListVariable : ListVariable<Quaternion>
+    {
+
+    }
+}
---
+++ Runtime/Definitions/QuaternionVariable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Toorah.ScriptableVariables
+{
+    [CreateAssetMenu(fileName = "Quaternion Variable", menuName = "Scriptable Variables/Single/Quaternion")]
+    public class QuaternionVariable : ScriptableVariable<Quaternion>
+    {
+
+    }
+}
